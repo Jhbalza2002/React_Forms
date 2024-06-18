945949131adcceaea220e0a92bcf7a8b33199b0e
Jhbalza2002 <josehumberto2002@gmail.com>
2024-06-18 17:44:01 -0500

    error messages updated

--- React_Forms_Project/src/components/Authenticate.jsx
+++ React_Forms_Project/src/components/Authenticate.jsx
@@ -5,9 +5,14 @@ import './Authenticate.css';
 export default function Authenticate({ token }) {
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
-  const [userData, setUserData] = useState(null); // State for user data
+  const [username, setUsername] = useState(null);
 
   async function handleClick() {
+    if (!token) {
+      setError("Please submit the form before trying to authenticate the token.");
+      return;
+    }
+
     try {
       const response = await axios.get(
         "https://fsa-jwt-practice.herokuapp.com/authenticate",
@@ -19,10 +24,11 @@ export default function Authenticate({ token }) {
         }
       );
 
-      const result = response.data;
+      const result = await response.data;
       console.log(result);
       setSuccessMessage(result.message);
-      setUserData(result.data);
+      setUsername(result.data.username);
+      setError(null);
     } catch (error) {
       setError(error.message);
     }
@@ -33,7 +39,7 @@ export default function Authenticate({ token }) {
       <h2>Authenticate!</h2>
       {error && <p className="authenticate-error">{error}</p>}
       {successMessage && <p className="authenticate-message">{successMessage}</p>}
-      {userData && <p className="authenticate-username">Username: {userData.username}</p>}
+      {username && <p className="authenticate-username">Username: {username}</p>}
       <button className="authenticate-button" onClick={handleClick}>
         Authenticate Token!
       </button>
